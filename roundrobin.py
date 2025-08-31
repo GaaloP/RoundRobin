b71794a5c83de3ecf3ae1165fbf344e73efa947a
GaaloP <gposada24@alumnos.uaq.mx>
2025-08-31 16:51:42 -0300

    se agregaron columnasal df

--- roundrobin.py
+++ roundrobin.py
@@ -2,16 +2,19 @@ import pandas as pd
 L = pd.DataFrame()
 L['PID'] = ['A','B','C','D','E','F']
 L['TS'] = [10,6,1,8,13,7]
-Laux = L
+L['TL'] = [0,0,0,0,0,0]
+L['TR'] = [0,0,0,0,0,0]
+L['TF'] = [0,0,0,0,0,0]
+L['TE'] = [0,0,0,0,0,0]
+Laux = L.copy()
 
 Q = 4
-Ti =1
+Ti = 1
 R = 0 #ESte es el reloj 
 Pant = pd.DataFrame()
 Pant['PID'] = []
 Pant['TS'] = []
 
-print('PENDEEEEEEJO',len(Laux))
 while Laux['TS'].sum() > 0:
     for i in range(len(Laux)):  
 
@@ -36,10 +39,18 @@ while Laux['TS'].sum() > 0:
 
                 R = R + Laux.loc[i, "TS"]
                 Laux.loc[i, "TS"] = 0
+                Laux.loc[i, "TF"] = R
+                #Tiempo de retorno = tiempo final - tiempo llegada (0)
+                Laux.loc[i, "TR"] = Laux.loc[i, "TF"] - Laux.loc[i, "TL"]
+                #Tiempo de espera = tiempo retorno - tiempo servicio
+                Laux.loc[i, "TE"] = Laux.loc[i, "TR"] - L.loc[i, "TS"]
             
     
     
         Pant = Laux.loc[i]
 
     print("Iteración con:\n", Laux)
+
+print("\nTiempo de espera promedio:\n", Laux['TE'].mean())
+print("\nTiempo de retorno promedio:\n", Laux['TR'].mean())
 print('Fin')
